b9833573817e5e84d4a8f381d026d6c6d6d479af
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-20 17:45:43 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace magic8ball
+{
+    class Program
+    {
+        public static Random rand = new Random();
+
+        public void Main(string[] args)
+        {
+
+        }
+    }
+}
