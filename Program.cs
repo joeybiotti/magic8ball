1fe2f748087f4b144cd40700c05d6d355f128c74
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-20 18:24:07 -0600

    added timeout

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace magic8ball
 {
@@ -27,6 +28,15 @@ namespace magic8ball
                     // changes console color, yells at user
                     Console.ForegroundColor =  ConsoleColor.Red;
                     Console.Write("No, really. Ask a question! ");
+                    DateTime _pause = DateTime.Now.AddSeconds(2);
+
+                    while(_pause > DateTime.Now)
+                    {
+                        Thread.Sleep(1);
+                    }
+
+                    Console.Clear();
+
                     continue;
                 }
                 int RandomNum = rand.Next(5);
@@ -35,32 +45,39 @@ namespace magic8ball
                 switch(RandomNum)
                 {
                     case 0:
+                        Console.ForegroundColor = ConsoleColor.Blue;
                         Console.WriteLine("Yes!");
                         break;
 
                     case 1:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
                         Console.WriteLine("Nah");
                         break;
 
                     case 2:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine("Maybe");
                         break;
 
                     case 3:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
                         Console.WriteLine("Nope");
                         break;
 
                     case 4:
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
                         Console.WriteLine("I don't think so");
                         break;
             }
 
+            Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("Press 'Q' to quit, or any other key if you've got more questions to ask! ");
             char input = Console.ReadKey().KeyChar;
 
             switch(input)
             {
                 case 'q':
+                    Console.ResetColor();
                     Console.Clear();
                     Environment.Exit(0);
                     break;
